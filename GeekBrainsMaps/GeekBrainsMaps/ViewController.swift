73505a5027397e60aa33a8b7dc68158119c6b826
Fvino <86255196+Fvino@users.noreply.github.com>
2021-09-27 13:00:01 +0200

    init

---
+++ GeekBrainsMaps/GeekBrainsMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeekBrainsMaps
+//
+//  Created by Valeriy Fomin on 27/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
